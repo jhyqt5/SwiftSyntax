c6ca4dfd9981d8acc8b447dd7545e7bda0938e25
Dan Huang <jhyqt5@gmail.com>
2016-06-08 21:20:18 -0600

    Use swift types strings, int, and bools

---
+++ SwiftTypes.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var integer: Int = 5
+var pi: Float = 3.14
+
+var strA = "Hello "
+var strB = "Daniel"
+
+var concatString = strA + strB
+
+concatString.dynamicType
+
+
